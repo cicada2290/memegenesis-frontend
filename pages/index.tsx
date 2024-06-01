279bc4502a8d5b206f5000e699b867a88de924a4
0xpokotaro <kentarou_masuda@ruckplus-tech.io>
2024-06-01 22:05:02 +0900

    💄 Fix UI/UX

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { title, subtitle } from '@/components/primitives'
 import DefaultLayout from '@/layouts/default'
+import { Accordion, AccordionItem } from "@nextui-org/react"
 
 export default function IndexPage() {
   return (
@@ -13,6 +14,19 @@ export default function IndexPage() {
             <h4 className={subtitle({ class: 'mt-4' })}>
               Beautiful, fast and modern React UI library.
             </h4>
+          </div>
+          <div className="w-96">
+            <Accordion variant="shadow">
+              <AccordionItem key="1" aria-label="Accordion 1" title="Day1" subtitle="Create wallet">
+                <p>Day1 content</p>
+              </AccordionItem>
+              <AccordionItem key="2" aria-label="Accordion 2" title="Day2" subtitle="Enable rippling">
+                <p>Day2 content</p>
+              </AccordionItem>
+              <AccordionItem key="3" aria-label="Accordion 3" title="Day3" subtitle="Mint memecoin">
+                <p>Day3 content</p>
+              </AccordionItem>
+            </Accordion>
           </div>
         </section>
       </DefaultLayout>
